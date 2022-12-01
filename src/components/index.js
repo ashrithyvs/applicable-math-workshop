765df11c3d7b1248a8cbb7fe89484f4d0afc93a4
asishtech <68698410+asishtech@users.noreply.github.com>
2022-12-01 11:26:38 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,9 @@
 export { default as Navbar } from "./Navbar";
 export { default as Hero } from "./Hero";
 export { default as About } from "./About";
-export { default as Organizers } from "./Organizers";
 export { default as Speakers } from "./Speakers";
+export { default as Organizers } from "./Organizers";
+
 export { default as Committee } from "./Committee";
 export { default as Registration } from "./Registration";
 export { default as Contact } from "./Contact";
